d906a8390e03c1f8ce1aec012c6595c98ce9ddbf
Jordan Ratanadawong <jordan.ratanadawong@gmail.com>
2016-04-29 09:05:30 -0700

    Added Player class to math_battle.rb

--- W1D4/math_battle.rb
+++ W1D4/math_battle.rb
@@ -1,29 +1,66 @@
-@p1_lives = 0
-@p2_lives = 0
+class Player
+	attr_accessor :name; :lives; :wins
+	def initialize(name, lives, wins)	
+		@name = name
+		@lives = lives
+		@wins = wins
+	end
+end
+
+@p1 = Player.new("", 0, 0)
+@p2 = Player.new("", 0, 0)
 
 def new_game
+	if @p1_name == ""
+		new_players
+	end
 	player1 = true
-	@p1_lives, @p2_lives = 3, 3
-	until @p1_lives == 0 || @p2_lives == 0
+	@p1[:lives], @p2[:lives] = 3, 3
+	puts "New Game!\n\n"
+	until @p1[:lives] == 0 || @p2[:lives] == 0
 		math_battle(player1)
 		player1 = !player1
 	end
-	if @p1_lives == 0 then puts "Player 2 wins!" else puts "Player 1 wins!" end
+	if @p1[:lives] == 0
+		puts "#{@p2[:name]} wins!" 
+		@p2[:wins] += 1
+		puts "#{@p1[:name]} Wins: #{@p1[:wins]}\n#{@p2[:name]} Wins: #{@p2[:wins]}"
+	else 
+		puts "#{@p1[:name]} wins!"
+		@p1[:wins] += 1
+		puts "#{@p1[:name]} Wins: #{@p1[:wins]}\n#{@p2[:name]} Wins: #{@p2[:wins]}"
+	end
+	play_again
 end
 
 def math_battle(player1)
 	digit1 = rand(1..20)
 	digit2 = rand(1..20)
-	if player1 then puts "Player 1" else puts "Player 2" end
+	if player1 then puts @p1[:name] else puts @p2[:name] end
 	puts "What is #{digit1} + #{digit2}?"
 	user_input = gets.chomp
 	if user_input.to_i == digit1 + digit2
 		puts "Correct!" 
 	else
 		puts "Wrong!"
-		player1 ? @p1_lives -= 1 : @p2_lives -= 1
-		if player1 then puts "Lives: #{@p1_lives}" else puts "Lives: #{@p2_lives}" end
+		player1 ? @p1[:lives] -= 1 : @p2[:lives] -= 1
+		if player1 then puts "Lives: #{@p1[:lives]}" else puts "Lives: #{@p2[:lives]}" end
 	end
 end
+def new_players
+	puts "Player 1, enter your name: "
+	@p1_name = gets
+	puts "Player 2, enter your name: "
+	@p2_name = gets
+end
 
-new_game
+def play_again
+	puts "Play again? 1.Yes 2.No"
+	user_input = gets.chomp.downcase
+	if user_input == "1" || user_input == "yes" then new_game else return "Thanks for playing!" end
+
+end
+
+
+
+puts new_game
