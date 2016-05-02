e7ad7b78a8a5fa4f23b908d49b8c0a1f02354427
Jordan Ratanadawong <jordan.ratanadawong@gmail.com>
2016-05-02 09:20:42 -0700

    fixed math_battle's new_game and new_players functions

--- W1D4/math_battle/game.rb
+++ W1D4/math_battle/game.rb
@@ -10,35 +10,38 @@ class Game
 	end
 
 	def new_game
+		@players_arr = []
 		puts "How many players do you have?"
 		new_players(gets.chomp.to_i)
-		@players_arr.size.times do |i|
-				@players_arr[i].lives = 3
-		end
+		start_game
+	end
+
+	def start_game
 		puts "\nNew Game!\n\n"
-		puts play_game
+		@players_arr.size.times do |i|
+			@players_arr[i].lives = 3
+		end
+		play_game
 		scoreboard
-		puts play_again
+		play_again
 	end
 
 	def play_game
 		cur_player = 0
 		alive_players = 0
 		until alive_players == 1
-			alive_players = 0
-			@players_arr.each { |player| alive_players += 1 if player.lives > 0 }
 			if @players_arr[cur_player].is_alive?
-				if alive_players == 1
-					@players_arr[cur_player].wins += 1
-					return "#{@players_arr[cur_player].name} wins!"
-				end
 			 	puts "#{@players_arr[cur_player].name}'s Turn\n"
 			 	puts math_battle(cur_player)
 			 	cur_player = next_turn(cur_player)
 			else
 				cur_player = next_turn(cur_player)
 			end
+			alive_players = 0
+			@players_arr.each { |player| alive_players += 1 if player.lives > 0 }
 		end
+		@players_arr[cur_player].wins += 1
+		puts "#{@players_arr[cur_player].name} wins!".white.on_red.blink
 	end
 
 	def scoreboard
@@ -52,10 +55,10 @@ class Game
 		answer = equation
 		user_input = gets.chomp
 		if user_input.to_i == answer
-			return "Correct!\n\n".green 
+			"Correct!\n\n".green 
 		else
 			@players_arr[cur_player].lives -= 1
-			return "Wrong!\n".red + "#{@players_arr[cur_player].name}'s Lives: #{@players_arr[cur_player].lives}\n\n"
+			"Wrong!\n".red + "#{@players_arr[cur_player].name}'s Lives: #{@players_arr[cur_player].lives}\n\n"
 		end
 	end
 
@@ -72,16 +75,12 @@ class Game
 		digit2 = rand(1..20)
 		operator = [:+, :-, :*].sample
 		puts "What is #{digit1} #{operator} #{digit2}?"
-		return digit1.send(operator, digit2)
+		digit1.send(operator, digit2)
 	end
 
-	def new_players(amount_of_players) #loop over number of players 
-		if @players_arr.size < amount_of_players
-			amount_of_players.times do
-				@players_arr << Player.new("", 0, 0)
-			end
-		end
+	def new_players(amount_of_players) 
 		amount_of_players.times do |cur_player|
+			@players_arr << Player.new("", 0, 0)
 			if @players_arr[cur_player].name == ""
 				puts "Please input your name:"
 				@players_arr[cur_player].name = gets.chomp
@@ -92,7 +91,17 @@ class Game
 	def play_again
 		puts "Play again? 1.Yes 2.No"
 		user_input = gets.chomp.downcase
-		if user_input == "1" || user_input == "yes" then new_game else return "Thanks for playing!" end
+ 		if user_input == "1" || user_input == "yes"
+ 		  puts "Same Players? 1. Yes 2. No"
+		 	input = gets.chomp.downcase 
+		 	if input == "1" || input == "no"
+		 		start_game
+		 	else
+		 		new_game
+		 	end
+ 		else
+ 			puts "Thanks for playing!" 
+ 		end
 	end
 end
 
